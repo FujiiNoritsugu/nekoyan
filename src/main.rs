7a06d79e68d86475a1b706da58bebd42d52cfc93
FujiiNoritsugu <quercus491@gmail.com>
2021-10-14 21:02:14 +0900

    ループの内側でスレッドを使用

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::thread;
 use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
 	for p in 0..=1000000{
-		pom(&p);
+        thread::spawn(
+            move || {
+		        pom(&p);
+            }
+        );
 	}
+
     let end = start.elapsed();
     println!("{}.{:03}秒経過しました。", end.as_secs(), end.subsec_nanos() / 1_000_000); 
 }
